457b9c2d9c7ec90aaa9dba613eaacce06effb495
KIM JUN SIK <silasoni94@gmail.com>
2022-11-15 09:45:29 +0900

    submit

--- users/tests.py
+++ users/tests.py
@@ -15,9 +15,8 @@ class UserRegistrationTest(APITestCase):
             "password": "password",
         }
         response = self.client.post(url, user_data)
-        # self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data["message"], self.data["message"])
-
+        self.assertEqual(response.data["username"], self.data["username"])
+        # 위와 같이 하는 것 같긴한데 정확한 답을 잘 모르겠습니다ㅠ
 
 class LoginUserTest(APITestCase):
     def setUp(self):
